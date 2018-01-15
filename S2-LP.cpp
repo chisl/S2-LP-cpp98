d58738ea27db19e52160ab6964a99c73c8067cd6
chisl <info@chisl.io>
2018-01-15 09:52:15 +0100

    Initial commit

---
+++ S2-LP.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        S2-LP
+ * description: Ultra-low power, high performance, sub-1GHz transceiver
+ * manuf:       ST Microelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/s2-lp.pdf
+ * date:        2018-01-15
+ * author       https://chisl.io/
+ * file:        S2-LP.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "S2-LP.hpp"
+
